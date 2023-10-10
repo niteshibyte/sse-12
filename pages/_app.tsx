dc1854f6eccd46cee41c3ee24fbd68fd5a03956b
nitesh <imnikku11@gmail.com>
2023-10-10 16:24:45 +0530

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,28 @@
+import { Provider } from 'react-redux'
 import "../styles/main.css";
 import '../styles/nitesh.css'
 import "../styles/additional-css-file.css"
+
 import type { AppProps } from 'next/app'
+import store from "../store/store";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import Head from 'next/head';
 import { ResponsiveProvider } from '@wonderflow/react-components';
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+  return <Provider store={store}>
 
-  return (
     <html lang="en" data-theme="light">
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/png" />
         <title>Wonderflow</title>
-          
-          <script>
-              {`
+
+        <script>
+          {`
                   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -21,16 +30,16 @@ function MyApp({ Component, pageProps }: AppProps) {
                   })(window,document,'script','dataLayer','GTM-PQ3W82L')
               `}
 
-          </script>
+        </script>
       </Head>
       <ResponsiveProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </ResponsiveProvider>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQ3W82L"
-                height="0" width="0" style={{ display: "none", visibility: 'hidden' }}></iframe>
-            </noscript>
+        height="0" width="0" style={{ display: "none", visibility: 'hidden' }}></iframe>
+      </noscript>
     </html>
-  )
+  </Provider>
 }
 
 export default MyApp
