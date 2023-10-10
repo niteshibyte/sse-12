3e1c19fc1f3e715bf05b85ce36f207b630cf915e
nitesh <imnikku11@gmail.com>
2023-10-10 15:50:20 +0530

    changes

---
+++ helper/html-parser.ts
@@ -0,0 +1,6 @@
+import parse from 'html-react-parser';
+
+ const Html_React_Parser=(data:any)=>{
+          return parse(data)
+}
+export default Html_React_Parser
---
+++ helper/htmlToTextConvert.tsx
@@ -0,0 +1,4 @@
+export default function htmlToTextConvert(data: any) {
+          return { __html: data };
+      }
+  
