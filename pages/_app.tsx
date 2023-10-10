f4a8e04b845a9c00f18d3bb9bc7bd0b74fcf7d53
nitesh <imnikku11@gmail.com>
2023-10-10 14:16:08 +0530

    add key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
+import "../styles/main.css";
+import '../styles/nitesh.css'
+import "../styles/additional-css-file.css"
+
 import type { AppProps } from 'next/app'
 
+import { useEffect } from 'react';
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <html lang="en" data-theme="light">
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/png" />
+        <title>Wonderflow</title>
+
+        <script>
+          {`
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-PQ3W82L')
+              `}
+
+        </script>
+      </Head>
+      {/* <ResponsiveProvider> */}
+        <Component {...pageProps} />
+      {/* </ResponsiveProvider> */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQ3W82L"
+        height="0" width="0" style={{ display: "none", visibility: 'hidden' }}></iframe>
+      </noscript>
+    </html>
+  )
 }
 
 export default MyApp
